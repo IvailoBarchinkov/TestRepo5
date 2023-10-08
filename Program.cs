1e686b6888250f284564fbe6a3f15a2adef6cadb
IvailoBarchinkov <146868852+IvailoBarchinkov@users.noreply.github.com>
2023-10-08 21:00:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GitTestRepository
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int cookiesCnt = int.Parse(Console.ReadLine());
+            List<string> cookies = new List<string>();
+            for (int i = 0; i < cookiesCnt; i++)
+            {
+                string cookie = Console.ReadLine();
+                cookies.Add(cookie);
+            }
+            //create method that deletes a given cookie from the list if it exists
+            //create method that adds a given cookie to the list if is doesnt exist 
+            //create method that gives all the cookies from the list
+
+        }
+    }
+}
