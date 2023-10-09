487c325e0d6feea616ac4994a8bd2ef29884aeac
IvailoBarchinkov <ivailobarchinkov2007@gmail.com>
2023-10-09 18:11:39 +0300

    new method added

--- Program.cs
+++ Program.cs
@@ -14,7 +14,18 @@
             //create method that deletes a given cookie from the list if it exists
             //create method that adds a given cookie to the list if is doesnt exist 
             //create method that gives all the cookies from the list
-
+            
+        }
+        static void DeleteCookie(string cookieName)
+        {
+            if(cookies.Contains(cookieName))
+            {
+                cookies.Remove(cookieName);
+            }
+            else
+            {
+             Console.WriteLine("There is no such cookie in the cookie box!");
+            }
         }
     }
 }
