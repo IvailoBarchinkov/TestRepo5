21b634d3b953494d5a1f468409bebe6a1536c4e0
IvailoBarchinkov <ivailobarchinkov2007@gmail.com>
2023-10-21 16:55:18 +0300

    testing

--- Program.cs
+++ Program.cs
@@ -4,45 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int cookiesCnt = int.Parse(Console.ReadLine());
-            List<string> cookies = new List<string>();
-            for (int i = 0; i < cookiesCnt; i++)
-            {
-                string cookie = Console.ReadLine();
-                cookies.Add(cookie);
-            }       
-            string cookieToDelete = Console.ReadLine();
-            string cookieToAdd = Console.ReadLine();
-            DeleteCookie(cookieToDelete);
-            AddCookie(cookieToAdd);
-            ListCookies();
+           System.Console.WriteLine("Hello, GitHub!");
             
         }
-        static void DeleteCookie(string cookieName)
-        {
-            if(cookies.Contains(cookieName))
-            {
-                cookies.Remove(cookieName);
-            }
-            else
-            {
-             Console.WriteLine("There is no such cookie in the cookie box!");
-            }
-        }
-        static void AddCookie(string cookieName)
-        {
-            if(!cookies.Contains(cookieName))
-            {
-                cookies.Add(cookieName);
-            }
-            else
-            {
-                COnsole.WriteLine("This cookie already exists in the cookie box!");
-            }
-        }      
-        static void ListCookies()
-        {
-            Console.WriteLine(string.Join(", ", cookies));
-        }
+        
     }
 }
